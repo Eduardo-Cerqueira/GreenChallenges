802609d54a297f54ecf6a7780658dcbbf3e6e18d
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2023-09-27 14:40:16 +0200

    :sparkles: Add redirect after form completion

--- src/Controller/ChallengeController.php
+++ src/Controller/ChallengeController.php
@@ -11,6 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route("/challenge")]
 class ChallengeController extends AbstractController
 {
+    #[Route("/", name: 'challengeIndex')]
+    public function index()
+    {
+        return $this->render('base.html.twig');
+    }
+
     #[Route("/create", name: 'challengeCreate')]
     public function createChallenge(Request $request)
     {
@@ -25,8 +31,31 @@ class ChallengeController extends AbstractController
 
             $em->persist($challenge);
             $em->flush();
+            
+            return $this->redirect($this->generateUrl('challengeIndex'));
+        }
 
-            //return $this->redirect($this->generateUrl('challenge.index'));
+        return $this->render('form/challenge.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route("/edit/{id}", name: 'challengeEdit')]
+    public function editChallenge($id, Request $request)
+    {
+        $challenge = new Challenge();
+
+        $form = $this->createForm(ChallengeType::class, $challenge);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($challenge);
+            $em->flush();
+            
+            return $this->redirect($this->generateUrl('challengeIndex'));
         }
 
         return $this->render('form/challenge.html.twig', [
