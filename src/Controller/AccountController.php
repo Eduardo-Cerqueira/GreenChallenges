67771b1de03121508b2da7cc3cb82cfe1316b3fe
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2023-09-29 02:54:38 +0200

    :lipstick: Update header and fix routes

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\RegisterType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,7 +11,7 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class LoginController extends AbstractController
+class AccountController extends AbstractController
 {
     #[Route("/register", name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $passwordHasher)
