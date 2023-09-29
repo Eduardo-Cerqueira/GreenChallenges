13faf27c55612aa40dd4e7c8f7b4f49401e41327
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2023-09-29 07:38:02 +0200

    :sparkles: Add chart to check values for current challenges status

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,7 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
 ];
---
+++ src/Controller/ChartjsController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\CurrentChallenge;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
+
+class ChartjsController extends AbstractController
+{
+    #[Route('/chartjs', name: 'app_chart')]
+    public function index(ChartBuilderInterface $chartBuilder): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        $doing = $em->getRepository(CurrentChallenge::class)->findBy(['status' => 'Doing']);
+        $completed = $em->getRepository(CurrentChallenge::class)->findBy(['status' => 'Completed']);
+        $timeout = $em->getRepository(CurrentChallenge::class)->findBy(['status' => 'Timeout']);
+        $abandoned = $em->getRepository(CurrentChallenge::class)->findBy(['status' => 'Abandoned']);
+
+
+
+        $chart = $chartBuilder->createChart(Chart::TYPE_DOUGHNUT);
+
+        $chart->setData([
+            'labels' => ['Doing', 'Completed', 'Timeout', 'Abandoned'],
+            'datasets' => [
+                [
+                    'label' => 'Users challenges',
+                    'backgroundColor' => ['rgb(255, 99, 132)', 'rgb(255, 99, 0)', 'rgb(255, 0, 132)', 'rgb(0, 99, 132)'],
+                    'borderColor' => 'rgb(255, 255, 255)',
+                    'data' => [sizeof($doing), sizeof($completed), sizeof($timeout), sizeof($abandoned)],
+                ],
+            ],
+        ]);
+
+        $chart->setOptions([
+            'scales' => [
+                'y' => [
+                    'suggestedMin' => 0,
+                    'suggestedMax' => 100,
+                ],
+            ],
+        ]);
+
+        return $this->render('chartjs.html.twig', [
+            'chart' => $chart,
+        ]);
+    }
+}
